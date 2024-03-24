138614508c6b30d0ae6fa577d8740be01f0206f3
ChangWan <wan723@outlook.com>
2024-03-24 20:09:06 +0000

    add hello check

--- src/main/java/com/backend/backend/BackendApplication.java
+++ src/main/java/com/backend/backend/BackendApplication.java
@@ -2,9 +2,17 @@ package com.backend.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BackendApplication {
+
+    @RequestMapping("/hello")
+    public String helloWorld() {
+        return "Hello, Soton!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
