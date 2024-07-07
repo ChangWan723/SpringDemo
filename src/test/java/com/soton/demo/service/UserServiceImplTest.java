aabaf93b6e79aadba9a1c41b24de24c83ee85389
ChangWan <wan723@outlook.com>
2024-07-07 21:11:15 +0100

    fix test

---
+++ src/test/java/com/soton/demo/service/UserServiceImplTest.java
@@ -0,0 +1,18 @@
+package com.soton.demo.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceImplTest {
+
+    @Test
+    void saveUser() {
+
+    }
+
+    @Test
+    void findAllUser() {
+
+    }
+}
